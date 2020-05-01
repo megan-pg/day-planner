c1ad5a7bce8043acd705a2e02f510447a0bd90e5
megan-pg <megan.pardygokcu@unh.edu>
2020-05-01 11:53:48 -0400

    changes made

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+//********OPEN CALENDER ************* */
+//Generate current day
+//Generate time blocks to user
+
+
+
+
+//**********TIMEBLOCKS FOR THAT DAY ************ */
+
+
+//******************ENTER THE EVENT TO THE CALENDER******* */
+
+
+//************SAVE THE EVENT TO CALENDAR ********* */
+
+
+
+//**************LOCAL STORAGE ********************* */
