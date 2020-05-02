27a558964006c54035dd1c5ca4f16546e95b8bfc
megan-pg <megan.pardygokcu@unh.edu>
2020-05-01 22:00:04 -0400

    added moment code and row with columns

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,32 @@
 //supporting documentation https://medium.com/@aleks.roslyakov/using-css-grid-jquery-making-a-daily-scheduler-pt-i-6ba0d2b37e6f
 //********OPEN CALENDER ************* */
 //Generate current day
-//Generate time blocks to user
+// the code from moment.js for day and time generation
+$(document).ready(function () {
+    var today = moment().format('MMMM Do YYYY, h:mm:ss a');
+    today.append();
+})
+var date = $("#date-field");
+date.text(today);
+//Generate time blocks to user IN HTML???????
 
 
 
 
 //**********TIMEBLOCKS FOR THAT DAY ************ */
 //9AM through 5PM
+//save buttons
+//current hour color
+//past hour color
+//future hour color
 
 
-//******************ENTER THE EVENT TO THE CALENDER******* */
+//******************ENTER AND EDIT THE EVENT TO THE CALENDER******* */
+//text enter field
+//append text to timeblock
+//Delete appended text Function
+
+
 //on click on the time block
 //pulled .on(click) from onclick activity in week 5
 $(document).ready(function () {
@@ -21,6 +37,7 @@ $(document).ready(function () {
 
 //************SAVE THE EVENT TO CALENDAR ********* */
 //Save Button appends the entered text to the timeblock
+//on click save button to push to local storage
 
 
 //**************LOCAL STORAGE ********************* */
