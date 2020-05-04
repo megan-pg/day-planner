8bf703c606d6598a13c539db3275e8237e860513
megan-pg <megan.pardygokcu@unh.edu>
2020-05-03 21:00:08 -0400

    tried to make the local storage work

--- assets/script.js
+++ assets/script.js
@@ -12,14 +12,11 @@ $(document).ready(function () {
 
 
 //**********TIMEBLOCKS FOR THAT DAY ************ */
-//9AM through 5PM
+//8AM through 6PM
+//text enter field
 //save buttons
-//current hour color
-//past hour color
-//future hour color
-//Generate time blocks to user IN HTML???????
-
-
+//for loop for to generate a row for every hour between 08:00 and 18:00 (aka 6pm because moment and time need 24 hour clocks to understand the time unit)
+//once the for loop cycles through every hour until it hits 18:00, it will stop generateing rows
 for (var i = 8; i <= 18; i++) {
     var hourTime = moment(i, "HH:mm:ss").format("h:mm:ss a");
     var timeDisplay;
@@ -28,76 +25,42 @@ for (var i = 8; i <= 18; i++) {
     } else {
         timeDisplay = "";
     }
-    //Trying to append per https://api.jquery.com/append/ and Class activities like the movie activity from
+    //Trying to append per https://api.jquery.com/append/ and Class activities like the movie activity and drinks activity
+    //textarea doesn't need a function because it is predefined
     $("#date-field").append(`
     <div class="row">
     <div class="hour">${hourTime}</div>
-    <textarea class="textarea">${""}</textarea>
+    <textarea event = ${hourTime} class="textarea">${""}</textarea>
     <button class="saveBtn">Save</button>
     </div>
 `)
-    console.log("#date-field".append);
 }
+//current hour color
+//past hour color
+//future hour color
+
 
 //******************ENTER AND EDIT THE EVENT TO THE CALENDER******* */
-//text enter field
+
 //append text to timeblock
 //Delete appended text Function
 
 
-//on click on the time block
-//pulled .on(click) from onclick activity in week 5
-// $(document).ready(function () {
-//     $("#click-me").on("click", function () {
-//         alert("I've been clicked!");
-//     });
-// });
-
-//************SAVE THE EVENT TO CALENDAR ********* */
+//************SAVE THE EVENT TO CALENDAR and LOCAL STORAGE ********* */
 //Save Button appends the entered text to the timeblock
 //on click save button to push to local storage
+//Save button sends the entered text to local storage
 $(document).on("click", ".saveBtn", function () {
-    var hour = $(this).prev().attr("hour");
-    var event = $(this).prev().val();
+    console.log("click")
+    event = "";
 
-    localStorage.setItem(hour, event);
+    var listOfThings = $(this).attr('save-event');
+
+    var enteredText = "#textarea" + listOfThings;
+    var event = $(enteredText).val();
+
+    var toDosArray = [listOfThings]
+
+    localStorage.setItem("text", JSON.stringify(toDosArray));
 
 });
-// $(document).on("click", "saveBtn", function () {
-//     console.log(document);
-//     console.log(saveBtn);
-//     if ($("input").first().val() === "correct") {
-//         $("span").text("Validated...").show();
-//         return;
-//     }
-
-//     $("span").text("Not valid!").show().fadeOut(1000);
-//     event.preventDefault();
-// });
-//     $(document).on('click', function (event) {
-//         localStorage.setItem('saved-text', JSON.stringify(savedEvent));
-//     });
-// }
-// function saveText(text) {
-//     // store the score, number of boxes, and the time in local storage
-
-//     // create a new score object with the three values that we want
-//     var enterdText = {
-//         text: [], //get the name from an input field
-//     };
-
-//     // add this saved string to the current scores
-//     // get all of the current scores from local storage
-//     var textField = getText();
-//     // push our scoreObject onto the existing scores array
-//     enteredText.push(text);
-//     // JSON.stringify to turn our array into a string
-//     var textJSON = JSON.stringify(textField);
-//     console.log(textJSON)
-//     // store our new JSON string in local store
-//     localStorage.setItem('event', textJSON);
-// }
-
-
-//**************LOCAL STORAGE ********************* */
-//Save button sends the entered text to local storage
